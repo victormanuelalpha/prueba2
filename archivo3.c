0c22eaea2c8b1ad23722a59d759bde012f689f88
victormanuelalpha <victor.ramirez@alphanetworks.mx>
2023-08-03 13:54:48 -0600

    se modifica el archivo3.c desde victor

--- archivo3.c
+++ archivo3.c
@@ -1,3 +1,8 @@
 print("Hola mundo 3")
 // Path: archivo4.c
 print("Hola mundo 4")
+// Path: archivo5.c
+print("Hola mundo 5")
+// Path: archivo6.c
+print("Hola mundo 6")
+// Path: modificado desde la rama victor
