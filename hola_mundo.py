99a7936773db53bfc284192a9ae615e3186e5942
jedua285 <jose.campuzano@alphanetworks.mx>
2023-08-04 11:27:40 -0600

    archivo subido desde branch jedua

---
+++ hola_mundo.py
@@ -0,0 +1,16 @@
+print('Hello, Word')
+print(f'prueba de commit')
+#git config --global user.name 'jedua285'
+#git config --global user.email 'jose.campuzano@alphanetworks.mx'
+#git config --global core.editor 'code .'
+#git init
+#git remote add origin https://github.com/victormanuelalpha/prueba2.git
+#git remote -v
+#fetch
+#git pull origin main
+#en la de archivos
+#git branch
+#git branch -m lalo jedua
+
+
+print('prueba de rama')
